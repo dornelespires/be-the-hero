421a0bb01f8d258eeff87c3097f8b5cab5f6c7ab
André <pires.andreld@gmail.com>
2020-03-27 22:47:59 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,58 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async create(request, response){
+        const {title, description, value} = request.body;
+        const ong_id = request.headers.authorization;
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
+        });
+
+        return response.json({id});
+    },
+
+    async index(request, response){
+        const {page = 1} = request.query;
+
+        const [count] = await connection.count().from('incidents');
+
+        const incidents = await connection.select([
+                'incidents.*', 
+                'ongs.name as ong_name', 
+                'ongs.whatsapp', 
+                'ongs.email',
+                'ongs.uf', 
+                'ongs.city'])
+            .from('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5);
+
+        response.header('X-Total-Count', count['count(*)']);
+
+        return response.json(incidents);
+    },
+
+    async delete(request, response){
+        const {id} = request.params; 
+        const ong_id = request.headers.authorization;
+
+        //primeiramente verifica se o id do elemento a ser deletado corresponde
+        //a um caso ligado à ONG logada no momento
+        const incident = await connection('incidents')
+            .where('id', id).select('ong_id').first();
+            
+        if(incident.ong_id !== ong_id ){
+            return response.status('401').json({ error: 'Operation not permitted'});
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return response.status(204).send();
+    }
+}
